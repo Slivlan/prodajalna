ddd1d680b35ffdc5faf538ac2542cb79e35a9e0b
Mihael Švigelj <ms0555@student.uni-lj.si>
2016-05-07 17:45:32 +0000

    Zakljucek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -201,6 +201,7 @@ streznik.post('/prijava', function(zahteva, odgovor) {
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
     var napaka2 = false;
+    var odziv = "Stranka je bila uspešno registrirana.";
     try {
       var stmt = pb.prepare("\
         INSERT INTO Customer \
@@ -208,14 +209,18 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
+      odziv = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
     }
-  
-    odgovor.end();
+    
+    vrniStranke(function(napaka1, stranke) {
+      vrniRacune(function(napaka2, racuni) {
+        odgovor.render('prijava', {sporocilo: odziv, seznamStrank: stranke, seznamRacunov: racuni});  
+      }) 
+    });
   });
 })
 
